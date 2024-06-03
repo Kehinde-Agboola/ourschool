9ea5ab1ca0073dbe0c900c753faa52a9a90333ba
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-06-03 14:13:37 +0100

    error-rectify

--- src/app/(client)/Home/Learning.tsx
+++ src/app/(client)/Home/Learning.tsx
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { LuArrowUpRight } from "react-icons/lu";
 import { Course } from "@/app/_constant/data";
 import Image from "next/image";
 import { Button } from "@/app/_components/button";
-import { useState } from "react";
 import {
   Autoplay,
   Keyboard,
@@ -16,20 +15,22 @@ import "swiper/css";
 import "swiper/css/virtual";
 import { MdArrowBack } from "react-icons/md";
 import { AiOutlineArrowRight } from "react-icons/ai";
+
 export const Learning = () => {
   const [prevEl, setPrevEl] = useState<HTMLElement | null>(null);
   const [nextEl, setNextEl] = useState<HTMLElement | null>(null);
+
   return (
     <main className="bg-learning relative bg-blue-100 bg-cover bg-center bg-no-repeat py-[4rem] px-8 md:px-0 text-white">
       <section>
         <div className="text-center">
           <h2>Have fun learning with videos</h2>
-          <p className="md:w-[60%] mx-auto  py-5">
+          <p className="md:w-[60%] mx-auto py-5">
             What we learn with joy, we never forget. Learning is more fun with
             recorded video lessons and animations that cover every subject.
           </p>
         </div>
-        <div className="">
+        <div>
           <Swiper
             modules={[Navigation, Pagination, Keyboard, Mousewheel, Autoplay]}
             mousewheel={true}
@@ -39,7 +40,6 @@ export const Learning = () => {
             navigation={{ prevEl, nextEl }}
             breakpoints={{
               0: { slidesPerView: 1 },
-
               640: {
                 slidesPerView: 2,
                 spaceBetween: 10,
@@ -50,33 +50,31 @@ export const Learning = () => {
               },
             }}
           >
-            {Course.map((d) => {
-              return (
-                <SwiperSlide>
-                  <div key={d?.title} className="relative">
-                    <Image src={d?.img} alt={d?.title} />
-                    <p className="absolute text-[12px] py-1 bg-black rounded-md px-3 top-0 right-0 m-5 md:m-3">
-                      {d?.category}
-                    </p>
-                  </div>
-                  <p className="text-[1.5rem] py-4">{d?.title}</p>
-                  <p className="text-[12px]">{d?.description}</p>
-                  <div className="flex items-center pt-4">
-                    <Button
-                      text="Play Video"
-                      className="bg-white text-black rounded-full relative px-10 py-3"
-                    />
-                    <LuArrowUpRight
-                      className="absolute left-[8rem]"
-                      color="black"
-                    />
-                  </div>
-                </SwiperSlide>
-              );
-            })}
+            {Course.map((d) => (
+              <SwiperSlide key={d.title}>
+                <div className="relative">
+                  <Image src={d.img} alt={d.title} />
+                  <p className="absolute text-[12px] py-1 bg-black rounded-md px-3 top-0 right-0 m-5 md:m-3">
+                    {d.category}
+                  </p>
+                </div>
+                <p className="text-[1.5rem] py-4">{d.title}</p>
+                <p className="text-[12px]">{d.description}</p>
+                <div className="flex items-center pt-4">
+                  <Button
+                    text="Play Video"
+                    className="bg-white text-black rounded-full relative px-10 py-3"
+                  />
+                  <LuArrowUpRight
+                    className="absolute left-[8rem]"
+                    color="black"
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
           </Swiper>
 
-          <div className=" hidden md:flex items-center justify-center gap-[3rem]  mt-[5rem]">
+          <div className="hidden md:flex items-center justify-center gap-[3rem] mt-[5rem]">
             <div
               ref={(node) => setPrevEl(node)}
               className="bg-white text-white h-12 w-12 rounded-full lg:flex items-center justify-center cursor-pointer"
@@ -85,7 +83,7 @@ export const Learning = () => {
             </div>
             <div
               ref={(node) => setNextEl(node)}
-              className="h-12 w-12 rounded-full lg:flex items-center justify-center cursor-pointer  bg-white text-white"
+              className="h-12 w-12 rounded-full lg:flex items-center justify-center cursor-pointer bg-white text-white"
             >
               <AiOutlineArrowRight color="blue" />
             </div>
