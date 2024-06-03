51dceb7a298d5eeca308774ce6cdf1cad509ef54
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-06-03 01:47:34 +0100

    data

---
+++ src/app/_constant/data.ts
@@ -0,0 +1,128 @@
+import { BsFacebook, BsInstagram, BsTwitter, BsYoutube } from "react-icons/bs";
+import Science from "../../../public/home/science.svg";
+import English from "../../../public/home/language.svg";
+import Math from "../../../public/home/mathematics.svg";
+
+export const navItem = [
+  {
+    name: "teacher",
+    path: "/teacher",
+  },
+  {
+    name: "library",
+    path: "/library",
+  },
+  {
+    name: "about",
+    path: "/about",
+  },
+  {
+    name: "donate",
+    path: "/donate",
+  },
+];
+
+export const Course = [
+  {
+    img: Science,
+    category: "Science",
+    title: "Basic Science",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Cursus et eu tellus aliquet libero et dui. Elementum ultrices tincidunt vitae. Love beau no carte et amor ",
+  },
+  {
+    img: English,
+    category: "Language",
+    title: "English Language",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Cursus et eu tellus aliquet libero et dui. Elementum ultrices tincidunt vitae. Love beau no carte et amor ",
+  },
+  {
+    img: Math,
+    category: "Mathematics",
+    title: "Mathematics",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Cursus et eu tellus aliquet libero et dui. Elementum ultrices tincidunt vitae. Love beau no carte et amor ",
+  },
+  {
+    img: Science,
+    category: "Science",
+    title: "Basic Science",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Cursus et eu tellus aliquet libero et dui. Elementum ultrices tincidunt vitae. Love beau no carte et amor ",
+  },
+  {
+    img: English,
+    category: "Language",
+    title: "English Language",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Cursus et eu tellus aliquet libero et dui. Elementum ultrices tincidunt vitae. Love beau no carte et amor ",
+  },
+  {
+    img: Math,
+    category: "Mathematics",
+    title: "Mathematics",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Cursus et eu tellus aliquet libero et dui. Elementum ultrices tincidunt vitae. Love beau no carte et amor ",
+  },
+];
+
+export const Service = [
+  {
+    name: "Teachers",
+    path: "/teacher",
+  },
+  {
+    name: "Donate",
+    path: "/donate",
+  },
+  {
+    name: "Projects",
+    path: "/projects",
+  },
+  {
+    name: "Library",
+    path: "/library",
+  },
+];
+export const Company = [
+  {
+    name: "About Us",
+    path: "/about",
+  },
+  {
+    name: "Help",
+    path: "/help",
+  },
+  {
+    name: "Terms and condition",
+    path: "/terms",
+  },
+  {
+    name: "Contact & Support",
+    path: "/contact",
+  },
+];
+
+export const socials = [
+  {
+    icon: BsFacebook,
+    size: 25,
+    color: "border-white b-2",
+  },
+  {
+    icon: BsTwitter,
+    size: 25,
+    color: "border-white b-2",
+  },
+  {
+    icon: BsYoutube,
+    size: 25,
+    color: "border-white b-2",
+  },
+  {
+    icon: BsInstagram,
+    size: 25,
+    color: "border-white b-2",
+  },
+];
