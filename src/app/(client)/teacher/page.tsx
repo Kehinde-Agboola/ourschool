e2f3f5741490c49cf85bbdcb0dce33a3b6a75f69
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-06-05 22:30:46 +0100

    page

--- src/app/(client)/teacher/page.tsx
+++ src/app/(client)/teacher/page.tsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import Nav from "./_components/Nav";
+import { Hero } from "./_components/Hero";
+import { OurSchool } from "./_components/OurSchool";
+import { Cards } from "./_components/Cards";
+import { Education } from "./_components/Education";
 const Teacher = () => {
-  return <div>Teacher page works</div>;
+  return (
+    <main className="">
+      <Nav />
+      <Hero />
+      <Cards />
+      <OurSchool />
+      <Education />
+    </main>
+  );
 };
 
 export default Teacher;
