3e7814e51ddf543d703e0e64be88bd2cf7c58d50
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-06-05 22:32:00 +0100

    data

--- src/app/_constant/data.ts
+++ src/app/_constant/data.ts
@@ -2,6 +2,9 @@ import { BsFacebook, BsInstagram, BsTwitter, BsYoutube } from "react-icons/bs";
 import Science from "../../../public/home/science.svg";
 import English from "../../../public/home/language.svg";
 import Math from "../../../public/home/mathematics.svg";
+import Analytics from "../../../public/teacher/analytics.svg";
+import Online from "../../../src/../public/teacher/learning.svg";
+import Management from "../../../public/teacher/management.svg";
 
 export const navItem = [
   {
@@ -70,6 +73,27 @@ export const Course = [
     title: "Mathematics",
     description:
       "Lorem ipsum dolor sit amet consectetur. Cursus et eu tellus aliquet libero et dui. Elementum ultrices tincidunt vitae. Love beau no carte et amor ",
+  },
+];
+
+export const Card = [
+  {
+    img: Analytics,
+    title: "E-learning analytics",
+    description:
+      "Get data-driven insights on students learning activities to curate education content for them and track performance. ",
+  },
+  {
+    img: Online,
+    title: "Personalized online learning",
+    description:
+      "Help a child get basic education from anywhere. OurSchool provides complete primary school education content for children to learn at their own pace ",
+  },
+  {
+    img: Management,
+    title: "Learning management",
+    description:
+      "Manage your students learning experiences. Track progress and  evaluate performance.",
   },
 ];
 
