bf123a4861ed97792e56404725c226294037e92f
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-06-05 22:28:40 +0100

    added-bg

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,12 +23,16 @@ const config: Config = {
           100: "#0964D7",
         },
         lightgray: "F3FBFF",
+        gradient:
+          " linear-gradient(to right bottom,rgba(39, 127, 238, 1),rgba(35, 119, 226, 1) rgba(9, 100, 215, 1)",
       },
       backgroundImage: {
         hero: "url('../../public/hero.svg')",
+        deserve: "url('../../public/teacher/deserve.svg')",
         learning: "url('../../public/learningbg.svg')",
         "hero-pattern":
           "linear-gradient(to right bottom, rgba('#277FEE',0.8), rgba('#2377E2',0.8), ('#0964D7', 0.8)), url('../../public/learningbg.svg')",
+        teacher: "url('../../public/teacher/teacher.svg')",
         footer: "url('../../public/footer.png')",
         "footer-pattern":
           "linear-gradient(to right bottom, rgba('#277FEE',0.8), rgba('#2377E2',0.8), ('#0964D7', 0.8)), url('../../public/footer.png')",
