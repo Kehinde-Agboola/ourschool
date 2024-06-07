963c14f16ca3b61d3b23b1c1664f1d92fad54115
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-06-07 17:43:28 +0100

    imported-components

--- src/app/(client)/library/page.tsx
+++ src/app/(client)/library/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import Nav from "./_components/Nav";
+import { Hero } from "./_components/Hero";
+import { Cards } from "./_components/Cards";
 const Library = () => {
-  return <div>Library page works</div>;
+  return (
+    <main className="bg-blue-100">
+      <Nav />
+      <Hero />
+      <Cards />
+    </main>
+  );
 };
 
 export default Library;
