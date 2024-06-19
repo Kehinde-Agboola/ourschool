ad0e6f0e73615127fe8a5e5fd769f6a804d77e48
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-06-19 01:14:20 +0100

    network-added

--- src/app/(client)/Home/Network.tsx
+++ src/app/(client)/Home/Network.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 import { Button } from "../../_components/button";
 export const Network = () => {
   return (
-    <main className="bg-gray py-[3rem] px-8 md:px-0">
+    <main className="bg-gray py-[3rem] px-8 md:px-0 font-popins">
       <section className="flex flex-col md:flex-row items-center justify-between">
         <Image src={network} alt="children" />
-        <div className=" w-full lg:w-[40%]">
-          <h2 className="text-center md:text-left pt-4">
+        <div className=" w-full lg:w-[45%]">
+          <h2 className="text-center font-bold  md:text-left pt-4">
             Enjoy <span className="text-blue-100">learning</span> anywhere
           </h2>
           <p className="text-dark-100 pt-4">
@@ -19,11 +19,11 @@ export const Network = () => {
           </p>
           <div className="flex flex-col md:flex-row items-center gap-5 pt-8">
             <Button
-              className="bg-blue-100 text-white rounded-full px-5 py-3"
+              className="bg-blue-100 text-white rounded-full px-[20px] py-[12px]"
               text="Download for Free"
             />
             <Button
-              className=" text-black border-blue-100 border-2 px-5 py-3 rounded-full"
+              className=" text-black border-blue-100 border-2 px-[20px] py-[12px] rounded-full outline-none"
               text="Upgrade your version "
             />
           </div>
