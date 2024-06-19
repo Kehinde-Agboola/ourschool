94aa46ff65c7c6beb60933dd2c0d1ff1bcfdd0c0
Kehinde-Agboola <kehindeagboola52@gmail.com>
2024-06-19 01:14:40 +0100

    library-page-added

---
+++ src/app/(client)/library/books/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./_components/Header";
+import Nav from "./_components/Nav";
+export default function page() {
+  return (
+    <div>
+      <Nav />
+      <Header />
+    </div>
+  );
+}
